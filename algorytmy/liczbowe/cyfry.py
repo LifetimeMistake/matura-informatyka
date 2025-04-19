fe8f722ab4864f15ca37b37615a83847c07e565c
LifetimeMistake <57274704+LifetimeMistake@users.noreply.github.com>
2025-04-19 15:29:57 +0000

    Add an optimization to the number decomposition algorithm

--- algorytmy/liczbowe/cyfry.py
+++ algorytmy/liczbowe/cyfry.py
@@ -24,6 +24,26 @@ def cyfry(n):
         
     return cyfry
 
+# Dobrze by było zwrócić uwagę na to, że powyższa funkcja wykorzystuje do swojego działania funkcję insert
+# Użycie insert(0, cyfra) jest nieefektywne, ponieważ:
+# 1. Każde wstawienie na początek listy wymaga przesunięcia wszystkich istniejących elementów
+# 2. Złożoność czasowa wynosi O(n) dla każdej operacji insert(0, ...)
+# 3. Dla n cyfr, całkowita złożoność wynosi O(n²)
+
+# Lepszym rozwiązaniem byłoby użycie append() (O(1) dla każdej operacji)
+# i odwrócenie listy na końcu (O(n)):
+def cyfry_optymalne(n):
+    if n == 0:
+        return [0]
+    n = abs(n)
+    cyfry = []
+    while n > 0:
+        cyfry.append(n % 10)
+        n = n // 10
+    return cyfry[::-1]
+
+# Ta wersja ma złożoność O(n) i jest znacznie szybsza dla dużych liczb.
+
 # Alternatywnie, gdy w rozwiązaniu dozwolone jest użycie zamiany typów danych
 def cyfry_str(n):
     return [int(c) for c in str(n)]
